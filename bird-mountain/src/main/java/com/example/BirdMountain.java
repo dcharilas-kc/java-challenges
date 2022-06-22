25a6406f0df4eda0d989b576e4ac7105bc6acc92
C0285129 <dimitris.charilas@santander.co.uk>
2022-06-22 16:50:23 +0300

    Initial code for bird challenge

--- bird-mountain/src/main/java/com/example/BirdMountain.java
+++ bird-mountain/src/main/java/com/example/BirdMountain.java
@@ -1,10 +1,78 @@
 package com.example;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.Objects.isNull;
+
 public class BirdMountain {
 
-    public int peakHeight(char[][] mountain) {
-        // TODO Your code here
-        return 1;
+    private static int height = 1;
+    private static char[][] mountain;
+    private static int remainingMountains;
+
+    public int peakHeight(char[][] input) {
+        mountain = input;
+        remainingMountains = countMountains();
+
+        while (remainingMountains > 0) {
+            scanMountain();
+            height++;
+        }
+        return height;
     }
+
+    private void scanMountain() {
+        for (int i = 0; i < mountain.length; i++) {
+            for (int j = 0; j < mountain[0].length; j++) {
+                if (isEdge(i, j)) {
+                    //TODO - check if this is valid
+                    mountain[i][j] = (char) height;
+                    remainingMountains--;
+                }
+            }
+        }
+    }
+
+    private boolean isEdge(int x, int y) {
+        List<Character> neighbors = getNeighbors(x, y);
+        return neighbors.stream().filter(n -> isNull(n) || ' ' == n.charValue()).count() > 0;
+    }
+
+    private boolean isActiveMountain(int x, int y) {
+        return '^' == mountain[x][y];
+    }
+
+    private List<Character> getNeighbors(int x, int y) {
+        List<Character> neighbors = new LinkedList<>();
+        // same row
+        neighbors.add(getElementValue(x - 1, y));
+        neighbors.add(getElementValue(x + 1, y));
+        // same column
+        neighbors.add(getElementValue(x, y - 1));
+        neighbors.add(getElementValue(x, y + 1));
+        return neighbors;
+    }
+
+    private Character getElementValue(int x, int y) {
+        return doesElementExist(x, y) ? mountain[x][y] : null;
+    }
+
+    private boolean doesElementExist(int x, int y) {
+        return x > 0 && x < mountain.length && y > 0 && y < mountain[0].length;
+    }
+
+    private int countMountains() {
+        int count = 0;
+        for (int i = 0; i < mountain.length; i++) {
+            for (int j = 0; j < mountain[0].length; j++) {
+                if (isActiveMountain(i,j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 
 }
