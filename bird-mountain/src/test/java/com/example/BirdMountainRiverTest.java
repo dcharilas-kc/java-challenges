3e7939f560d0a645d2b84151d5679d65a3cd223c
C0285129 <dimitris.charilas@santander.co.uk>
2022-06-27 20:11:59 +0300

    Adds tests for bird challenge part 2

--- bird-mountain/src/test/java/com/example/BirdMountainRiverTest.java
+++ bird-mountain/src/test/java/com/example/BirdMountainRiverTest.java
@@ -3,13 +3,11 @@ package com.example;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class BirdMountainRiverTest {
 
     @Test
     public void test1() {
-        BirdMountainRiver birdMountain = new BirdMountainRiver();
         char[][] terrain = {
                 "  ^^^^^^             ".toCharArray(),
                 "^^^^^^^^       ^^^   ".toCharArray(),
@@ -22,12 +20,11 @@ public class BirdMountainRiverTest {
                 "^^^^^^^^     ^     ^ ".toCharArray(),
                 "^^^^^        ^^^^^^^ ".toCharArray()
         };
-        assertArrayEquals(new int[]{189,99,19,3}, birdMountain.dryGround(terrain));
+        assertArrayEquals(new int[]{189,99,19,3}, BirdMountainRiver.dryGround(terrain));
     }
 
     @Test
     public void test_noriver() {
-        BirdMountainRiver birdMountain = new BirdMountainRiver();
         char[][] terrain = {
                 "  ^^^^^^             ".toCharArray(),
                 "^^^^^^^^       ^^^   ".toCharArray(),
@@ -39,9 +36,66 @@ public class BirdMountainRiverTest {
                 "^^^^^^^^     ^     ^ ".toCharArray(),
                 "^^^^^        ^^^^^^^ ".toCharArray()
         };
-        //TODO verify this is the expected result in case of no river
-        assertArrayEquals(new int[]{189,189,189,189}, birdMountain.dryGround(terrain));
+        //TODO configure assert - only 1st element is known
+        //assertArrayEquals(new int[]{189,0,0,0}, BirdMountainRiver.dryGround(terrain));
     }
 
+    @Test
+    public void test_nomountain() {
+        char[][] terrain = {
+                "          ".toCharArray(),
+                "          ".toCharArray(),
+                "          ".toCharArray(),
+                "----------".toCharArray(),
+                "          ".toCharArray(),
+                "          ".toCharArray(),
+                "          ".toCharArray()
+        };
+        //TODO configure assert - only 1st element is known
+        //assertArrayEquals(new int[]{60,0,0,0}, BirdMountainRiver.dryGround(terrain));
+    }
+
+    @Test
+    public void test_volcano() {
+        char[][] terrain = {
+                "---------------------".toCharArray(),
+                "      ^^^^^^^^^      ".toCharArray(),
+                "    ^^^^^^^^^^^^^    ".toCharArray(),
+                "  ^^^^^^^^^^^^^^^^^  ".toCharArray(),
+                " ^^^^^^^     ^^^^^^^ ".toCharArray(),
+                "^^^^^^^       ^^^^^^^".toCharArray(),
+                "^^^^^^^       ^^^^^^^".toCharArray(),
+                "^^^^^^^       ^^^^^^^".toCharArray(),
+                " ^^^^^^^     ^^^^^^^ ".toCharArray(),
+                "  ^^^^^^^^^^^^^^^^^  ".toCharArray(),
+                "    ^^^^^^^^^^^^^    ".toCharArray(),
+                "      ^^^^^^^^^      ".toCharArray()
+        };
+        //TODO configure assert - only 1st element is known
+        //assertArrayEquals(new int[]{231,0,0,0}, BirdMountainRiver.dryGround(terrain));
+    }
+
+    @Test
+    public void test_lake() {
+        char[][] terrain = {
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^-----^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^-----^^^^^^^^^^".toCharArray(),
+                "------------------^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^-----^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^-----^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^".toCharArray()
+        };
+        //TODO configure assert - only 1st element is known
+        //assertArrayEquals(new int[]{231,0,0,0}, BirdMountainRiver.dryGround(terrain));
+    }
 }
 
