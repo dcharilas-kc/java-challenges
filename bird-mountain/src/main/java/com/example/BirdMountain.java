623472b3b67a30686d627f44e7f650b754f676b0
C0285129 <dimitris.charilas@santander.co.uk>
2022-06-27 17:59:56 +0300

    Updates solution

--- bird-mountain/src/main/java/com/example/BirdMountain.java
+++ bird-mountain/src/main/java/com/example/BirdMountain.java
@@ -13,39 +13,63 @@ public class BirdMountain {
     private static char[][] mountain;
     private static long remainingMountains;
 
-    public int peakHeight(char[][] input) {
+    public static int peakHeight(char[][] input) {
+        height = 1;
         mountain = input;
         remainingMountains = countMountains();
 
         while (remainingMountains > 0) {
+            System.out.println("Remaining mountains: " + remainingMountains);
             scanMountain();
+            printMountain();
             height++;
         }
-        return height;
+        return height-1;
     }
 
-    private void scanMountain() {
+    private static void printMountain() {
+        for (int i = 0; i < mountain.length; i++) {
+            String line = "";
+            for (int j = 0; j < mountain[0].length; j++) {
+                line += mountain[i][j] + " ";
+            }
+            System.out.println(line);
+        }
+    }
+
+    private static void scanMountain() {
+        char[][] mountainUpdated = cloneMountain(mountain);
         for (int i = 0; i < mountain.length; i++) {
             for (int j = 0; j < mountain[0].length; j++) {
-                if (isEdge(i, j)) {
-                    //TODO - check if this is valid
-                    mountain[i][j] = (char) height;
+                if (isActiveMountain(i,j) && isEdge(i, j)) {
+                    mountainUpdated[i][j] = Character.forDigit(height,10);
                     remainingMountains--;
                 }
             }
         }
+        mountain = cloneMountain(mountainUpdated);
     }
 
-    private boolean isEdge(int x, int y) {
+    private static char[][] cloneMountain(char[][] mountainInput) {
+        char[][] copiedMountain = new char[mountainInput.length][mountainInput[0].length];
+        for (int i = 0; i < mountainInput.length; i++) {
+            for (int j = 0; j < mountainInput[0].length; j++) {
+                copiedMountain[i][j] = mountainInput[i][j];
+            }
+        }
+        return copiedMountain;
+    }
+
+    private static boolean isEdge(int x, int y) {
         List<Character> neighbors = getNeighbors(x, y);
-        return neighbors.stream().filter(n -> isNull(n) || ' ' == n.charValue()).count() > 0;
+        return neighbors.stream().filter(n -> isNull(n) || '^' != n.charValue()).count() > 0;
     }
 
-    private boolean isActiveMountain(int x, int y) {
+    private static boolean isActiveMountain(int x, int y) {
         return '^' == mountain[x][y];
     }
 
-    private List<Character> getNeighbors(int x, int y) {
+    private static List<Character> getNeighbors(int x, int y) {
         List<Character> neighbors = new LinkedList<>();
         // same row
         neighbors.add(getElementValue(x - 1, y));
@@ -56,25 +80,24 @@ public class BirdMountain {
         return neighbors;
     }
 
-    private Character getElementValue(int x, int y) {
+    private static Character getElementValue(int x, int y) {
         return doesElementExist(x, y) ? mountain[x][y] : null;
     }
 
-    private boolean doesElementExist(int x, int y) {
-        return x > 0 && x < mountain.length && y > 0 && y < mountain[0].length;
+    private static boolean doesElementExist(int x, int y) {
+        return x >= 0 && x < mountain.length && y >= 0 && y < mountain[0].length;
     }
 
-    private long countMountains() {
+    private static long countMountains() {
         return Stream.of(mountain)
                 .flatMapToInt(cells -> IntStream.range(0, cells.length).filter(i -> cells[i] == '^'))
                 .count();
     }
 
     /** used for testing  */
-    public long countMountains(char[][] input) {
+    public static long countMountains(char[][] input) {
         mountain = input;
         return countMountains();
     }
-
 
 }
