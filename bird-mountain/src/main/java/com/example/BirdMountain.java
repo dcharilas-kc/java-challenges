aaac324b56be3e5480f27cb3cc636e9d79ab5b0f
C0285129 <dimitris.charilas@santander.co.uk>
2022-06-27 18:02:33 +0300

    Adds tests for bird challenge part 1

--- bird-mountain/src/main/java/com/example/BirdMountain.java
+++ bird-mountain/src/main/java/com/example/BirdMountain.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class BirdMountain {
 
-    public int peakHeight(char[][] mountain) {
+    public static int peakHeight(char[][] mountain) {
         // TODO Your code here
         return 1;
     }
--- bird-mountain/src/test/java/com/example/BirdMountainTest.java
+++ bird-mountain/src/test/java/com/example/BirdMountainTest.java
@@ -6,18 +6,113 @@ import static org.junit.Assert.assertEquals;
 
 public class BirdMountainTest {
 
+    char[][] MOUNTAIN = {
+            "^^^^^^        ".toCharArray(),
+            " ^^^^^^^^     ".toCharArray(),
+            "  ^^^^^^^     ".toCharArray(),
+            "  ^^^^^       ".toCharArray(),
+            "  ^^^^^^^^^^^ ".toCharArray(),
+            "  ^^^^^^      ".toCharArray(),
+            "  ^^^^        ".toCharArray()
+    };
+
+    char[][] ONE = {"  ^^  ".toCharArray()};
+
+    char[][] ZERO = {"    ".toCharArray()};
+
+    char[][] RANDOM = {
+            "                          ^^^^^^^^^^^^^^^^        ".toCharArray(),
+            "                           ^^^^^^^^^^^^^          ".toCharArray(),
+            "                         ^^^^^^^^^^^^^^^          ".toCharArray(),
+            "                           ^^^^^^^^^^^^^          ".toCharArray(),
+            "                         ^^^^^^^^^^^^^^           ".toCharArray(),
+            "                       ^^^^^^^^^^^^^^^            ".toCharArray(),
+            "                      ^^^^^^^^^^^^^^              ".toCharArray(),
+            "                      ^^^^^^^^^^^^^^              ".toCharArray(),
+            "                       ^^^^^^^^^^^^               ".toCharArray(),
+            "                        ^^^^^^^^^                 ".toCharArray(),
+            "                         ^^^^^^^                  ".toCharArray(),
+            "                          ^^^^^^                  ".toCharArray(),
+            "                           ^^^^^^                 ".toCharArray(),
+            "                            ^^^^^                 ".toCharArray(),
+            "                            ^^^                   ".toCharArray(),
+            "                            ^                     ".toCharArray(),
+            "                                                  ".toCharArray(),
+            "                           ^^^                    ".toCharArray(),
+            "                          ^^^^^^                  ".toCharArray(),
+            "                           ^^^                    ".toCharArray(),
+            "                          ^^^                     ".toCharArray(),
+            "                          ^^^                     ".toCharArray(),
+            "                         ^^^^^                    ".toCharArray(),
+            "                         ^^^^^^                   ".toCharArray(),
+            "                         ^^^^^^^^                 ".toCharArray()
+    };
+
+    char[][] PLATEAU = {
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray(),
+            "^^^^^^^^^^^^^^^^^^^^^".toCharArray()
+    };
+
+    char[][] VOLCANO = {
+            "      ^^^^^^^^^      ".toCharArray(),
+            "    ^^^^^^^^^^^^^    ".toCharArray(),
+            "  ^^^^^^^^^^^^^^^^^  ".toCharArray(),
+            " ^^^^^^^     ^^^^^^^ ".toCharArray(),
+            "^^^^^^^       ^^^^^^^".toCharArray(),
+            "^^^^^^^       ^^^^^^^".toCharArray(),
+            "^^^^^^^       ^^^^^^^".toCharArray(),
+            " ^^^^^^^     ^^^^^^^ ".toCharArray(),
+            "  ^^^^^^^^^^^^^^^^^  ".toCharArray(),
+            "    ^^^^^^^^^^^^^    ".toCharArray(),
+            "      ^^^^^^^^^      ".toCharArray()
+    };
+
     @Test
-    public void test1() {
-        BirdMountain birdMountain = new BirdMountain();
-        char[][] mountain = {
-                "^^^^^^        ".toCharArray(),
-                " ^^^^^^^^     ".toCharArray(),
-                "  ^^^^^^^     ".toCharArray(),
-                "  ^^^^^       ".toCharArray(),
-                "  ^^^^^^^^^^^ ".toCharArray(),
-                "  ^^^^^^      ".toCharArray(),
-                "  ^^^^        ".toCharArray()
-        };
-        assertEquals(3, birdMountain.peakHeight(mountain));
+    public void test_default() {
+        assertEquals(3, BirdMountain.peakHeight(MOUNTAIN));
+    }
+
+    @Test
+    public void test_big_mountain() {
+        assertEquals(6, BirdMountain.peakHeight(RANDOM));
+    }
+
+    @Test
+    public void test_one() {
+        assertEquals(1, BirdMountain.peakHeight(ONE));
+    }
+
+    @Test
+    public void test_zero() {
+        assertEquals(0, BirdMountain.peakHeight(ZERO));
+    }
+
+    @Test
+    public void test_plateau() {
+        assertEquals(11, BirdMountain.peakHeight(PLATEAU));
+    }
+
+    @Test
+    public void test_volcano() {
+        assertEquals(4, BirdMountain.peakHeight(VOLCANO));
     }
 }
