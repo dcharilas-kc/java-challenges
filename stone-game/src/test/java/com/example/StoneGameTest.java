0072166b4cab8a472ca2e18ef73aeff9ba6b5ee3
Mark Schnitzius <mark.schnitzius@kinandcarta.com>
2022-07-12 10:41:17 +0100

    More efficient solution.

--- stone-game/src/test/java/com/example/StoneGameTest.java
+++ stone-game/src/test/java/com/example/StoneGameTest.java
@@ -24,8 +24,18 @@ public class StoneGameTest {
     }
 
     @Test
+    public void testBobOddLengthButAliceWins() {
+        assertTrue(stoneGame.stoneGame(new int[]{1, 5, 5}));
+    }
+
+    @Test
     public void test_biglist() {
         assertTrue(stoneGame.stoneGame(new int[]{5, 17, 19, 1, 15, 19, 11, 9, 6, 5, 11, 7, 12, 12, 2, 14, 3, 6, 18, 3}));
+    }
+
+    @Test
+    public void test_verybiglist() {
+        assertTrue(stoneGame.stoneGame(new int[]{7, 7, 12, 16, 41, 48, 41, 48, 11, 9, 34, 2, 44, 30, 27, 12, 11, 39, 31, 8, 23, 11, 47, 25, 15, 23, 4, 17, 11, 50, 16, 50, 38, 34, 48, 27, 16, 24, 22, 48, 50, 10, 26, 27, 9, 43, 13, 42, 46, 24}));
     }
 
 }
