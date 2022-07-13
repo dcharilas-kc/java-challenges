149cfa89898b58070da99caf0d8b5b75b54d18cd
C0285129 <dimitris.charilas@santander.co.uk>
2022-07-13 11:36:28 +0300

    Adds code to generate treenode object

--- binary-tree-cameras/src/main/java/com/example/Camera.java
+++ binary-tree-cameras/src/main/java/com/example/Camera.java
@@ -2,12 +2,6 @@ package com.example;
 
 public class Camera {
 
-    public int minCameraCover(Integer[] tree) {
-        TreeNode treeNode = null;
-        // TODO construct tree node
-        return minCameraCover(treeNode);
-    }
-
     public int minCameraCover(TreeNode root) {
         //TODO your code here
         return 0;
