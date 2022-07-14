6882a8e061c8d32007564682502ae35ba48bb2b3
Mark Schnitzius <mark.schnitzius@kinandcarta.com>
2022-07-14 10:23:20 +0100

    Fixed binary-tree-camera algorithm

--- binary-tree-cameras/src/main/java/com/example/Camera.java
+++ binary-tree-cameras/src/main/java/com/example/Camera.java
@@ -4,36 +4,40 @@ public class Camera {
 
     public int minCameraCover(TreeNode root) {
 
-        int minCameras = minCamerasNeeded(root).minCameras;
-        return minCameras == 0 ? 1 : minCameras;
+        Result rootResult = minCamerasNeeded(root);
+        // If the root node ends up not covered by a camera on one of its children, we need
+        // to put a camera on it.
+        return rootResult.minCameras + (rootResult.isCovered ? 0 : 1);
     }
 
     private Result minCamerasNeeded(TreeNode node) {
 
         // Easiest to just handle null children here instead of checking before recursing.
         if (node == null) {
-            return new Result(false, 0);
+            return new Result(false, true,0);
         }
 
         // If it's a leaf node, it's never necessary to have a camera; we can just put one
         // on its parent.
         if (node.left == null && node.right == null) {
-            return new Result(false, 0);
+            return new Result(false, false, 0);
         }
 
         // Traverse both children.
         Result leftResult = minCamerasNeeded(node.left);
         Result rightResult = minCamerasNeeded(node.right);
 
-        // If neither the left nor the right child has a camera, then we'll want one on this node.
-        boolean needsCamera = !leftResult.nodeHasCamera && !rightResult.nodeHasCamera;
-        return new Result(needsCamera, leftResult.minCameras + rightResult.minCameras + (needsCamera ? 1 : 0));
+        // If either the left or the right child is not yet covered, we'll need a camera on this node.
+        boolean needsCamera = !leftResult.isCovered || !rightResult.isCovered;
+        boolean isCovered = needsCamera || leftResult.hasCamera || rightResult.hasCamera;
+        return new Result(needsCamera, isCovered, leftResult.minCameras + rightResult.minCameras + (needsCamera ? 1 : 0));
     }
 
     /**
-     * A record class, because our method needs to return two values (whether we put a camera on
-     * the given node, and the minimum number of cameras that the tree under the given node required).
+     * A record class, because our method needs to return three values (whether the node has a camera,
+     * whether it's covered by a camera on one of its children, and the minimum number of cameras that
+     * the tree under the given node required).
      */
-    private record Result(boolean nodeHasCamera, int minCameras) {
+    private record Result(boolean hasCamera, boolean isCovered, int minCameras) {
     }
 }
