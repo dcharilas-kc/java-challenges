17d6d63beb89774be5cfb9eb70ea0e0b3a9421c3
Dimitris Charilas <dimitris.charilas@kinandcarta.com>
2022-07-14 14:14:05 +0300

    Adds stream challenge

---
+++ stream-practice/src/main/java/com/example/part1/Exercise.java
@@ -0,0 +1,32 @@
+package com.example.part1;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Exercise {
+
+    public int count(IntStream intStream) {
+        //TODO add code here
+        return 0;
+    }
+
+    public IntStream increment(IntStream intStream) {
+        //TODO add code here
+        return intStream;
+    }
+
+    public int sum(IntStream intStream) {
+        //TODO add code here
+        return 0;
+    }
+
+    public IntStream filter(IntStream intStream) {
+        //TODO add code here
+        return intStream;
+    }
+
+    public String concatenate(Stream<String> stream) {
+        //TODO add code here
+        return "";
+    }
+}
